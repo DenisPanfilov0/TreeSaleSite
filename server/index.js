b236dec693aa90e847b0c1dc0eeba963e0a8bbf0
DenisPanfilov0 <123986275+DenisPanfilov0@users.noreply.github.com>
2023-11-30 23:18:13 +0300

    Add data base mongoDB

--- server/index.js
+++ server/index.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const app = express();
 const port = 3000; 
+const mongoose = require('mongoose');
+const User = require('./src/UserModel.tsx');
+const { exec } = require('child_process');
+
+
+mongoose.connect('mongodb+srv://Alexander:1q2w3e4r@cluster0.jwcpdzh.mongodb.net/all_users', {
+  // useUnifiedTopology: true,
+  // useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Ошибка подключения к MongoDB:'));
+db.once('open', async () => {
+  console.log('Успешное подключение к MongoDB');
+
+  const newUser = new User({
+    id: '2',
+    username: 'Petr',
+    email: 'petr@gmail.com',
+    password: '1q2w3e',
+  });
+
+  try {
+    await newUser.save();
+    console.log('Пользователь успешно добавлен в коллекцию');
+  } catch (error) {
+    console.error('Ошибка при сохранении пользователя:', error);
+  }
+});
+
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // Разрешает запросы с этого origin
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
 
 app.get('/', (req, res) => {
   res.send('Привет, мир!');
