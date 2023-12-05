43e7094f64f68eb65e48f1515df356091b469578
DenisPanfilov0 <123986275+DenisPanfilov0@users.noreply.github.com>
2023-12-05 20:45:35 +0300

    Update

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 3000; 
 const mongoose = require('mongoose');
 const User = require('./src/UserModel.tsx');
-const { exec } = require('child_process');
-
+const bcrypt = require('bcrypt');
 
 mongoose.connect('mongodb+srv://Alexander:1q2w3e4r@cluster0.jwcpdzh.mongodb.net/all_users', {
   // useUnifiedTopology: true,
@@ -17,28 +16,65 @@ db.on('error', console.error.bind(console, 'Ошибка подключения 
 db.once('open', async () => {
   console.log('Успешное подключение к MongoDB');
 
-  const newUser = new User({
-    id: '2',
-    username: 'Petr',
-    email: 'petr@gmail.com',
-    password: '1q2w3e',
-  });
+  // const newUser = new User({
+  //   id: '2',
+  //   username: 'Lala',
+  //   email: 'lala@gmail.com',
+  //   password: 'Lala123',
+  // });
 
-  try {
-    await newUser.save();
-    console.log('Пользователь успешно добавлен в коллекцию');
-  } catch (error) {
-    console.error('Ошибка при сохранении пользователя:', error);
+  // try {
+    
+    
+  //   await newUser.save();
+  //   console.log('Пользователь успешно добавлен в коллекцию');
+  // } catch (error) {
+  //   console.error('Ошибка при сохранении пользователя:', error);
+  // }
+
+  const login = 'lala@gmail.com';
+  const password = 'Lala123';
+
+  const existingUser = await User.findOne({ email: login });
+  console.log(existingUser);
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+
+    if (isPasswordValid) {
+      const token = existingUser.generateAuthToken();
+      console.log('Авторизация успешна. Токен:', token);
+    } else {
+      console.error('Ошибка при авторизации. Неверный пароль');
+    }
+  } else {
+    console.error('Ошибка при авторизации. Пользователь не найден');
   }
-});
 
+  const loginToFind = 'petr@gmail.com';
+const userWithEmail = await User.findOne({ email: loginToFind }, {password: 1});
+if (userWithEmail.password == password) {
+  console.log('всё получилось')
+}
+// console.log(userWithEmail);
+
+
+// const users = await User.find({ email: 'exampleUser' }, { name: 1, email: 1 });
+// console.log(users);
+
+// console.log(await User.find({name: 1}, { email: loginToFind }));
+
+// if (userWithEmail) {
+//   console.log('Найден пользователь с email', loginToFind, 'и _id', userWithEmail._id);
+// } else {
+//   console.log('Пользователь с email', loginToFind, 'не найден');
+// }
+});
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
 
 app.get('/', (req, res) => {
   res.send('Привет, мир!');
--- server/src/UserModel.tsx
+++ server/src/UserModel.tsx
@@ -1,12 +1,40 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-    id: String,
-    username: String,
-    email: String,
-    password: String,
+  id: String,
+  username: String,
+  email: String,
+  password: String,
 });
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+  
+    try {
+      const salt = await bcrypt.genSalt(10);
+  
+      if (user.password !== null && user.password !== undefined) {
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        user.password = hashedPassword;
+      }
+  
+      next();
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+// Метод для генерации JWT
+userSchema.methods.generateAuthToken = function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id }, 'your-secret-key'); // Замените на свой секретный ключ
+
+  return token;
+};
 
 const User = mongoose.model('User', userSchema);
 
+// export default User;
 module.exports = User;
