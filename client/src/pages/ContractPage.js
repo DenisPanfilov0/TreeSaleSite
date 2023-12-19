31227527b77da15c8267e760ebdde4b1e3e28bc2
DenisPanfilov0 <123986275+DenisPanfilov0@users.noreply.github.com>
2023-12-19 19:56:29 +0300

    update

--- client/src/pages/ContractPage.js
+++ client/src/pages/ContractPage.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import './ContractPage.css'; // Создайте файл для стилей
+import './ContractPage.css';
+import './TextImage.css';
+
+
+// const ImageOverlay = ({ text }) => (
+//   <div className="contract_date">
+//     <p>{text}</p>
+//     {/* Другие элементы или текст */}
+//   </div>
+// );
+
 
 const ContractPage = ({ user, order }) => {
+
+  const currentDate = new Date();
+  const formattedDate = `${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate.getFullYear()}`;
+
   return (
     <div className="contract-page">
       <div className="contract-content">
@@ -14,7 +28,10 @@ const ContractPage = ({ user, order }) => {
 
       <div className="image-scroll-container">
         <img src="/image/dogovor_po_drovam2_page-0001.jpg" alt="Image 1" />
+        <div className="contract_date">{formattedDate}</div>
+        {/* <div className="delivery_address"><p>"Адрес Доставки"</p></div> */}
         <img src="/image/dogovor_po_drovam2_page-0002.jpg" alt="Image 2" />
+        {/* <ImageOverlay text="Текст поверх изображения 2" /> */}
       </div>
     </div>
   );
