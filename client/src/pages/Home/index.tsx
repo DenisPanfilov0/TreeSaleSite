556c5bb11ddd06d75aaf5acaaec8ca13a2b9a090
DenisPanfilov0 <123986275+DenisPanfilov0@users.noreply.github.com>
2023-12-26 20:41:20 +0300

    update

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
-/*//Этот файл представляет собой компонент React, отвечающий за отображение домашней страницы веб-приложения, 
-//содержащей две секции с изображениями и текстовым описанием, представляющими товары и услуги компании.*/
 import React from 'react';
 import { Row, Col, Typography, Button } from 'antd';
+import { useNavigate } from 'react-router-dom'; // Импортируем useNavigate
 import firstImage from './FirstImage.jpg';
 import secondImage from './55555666666.jpg';
 
 const HomePage: React.FC = () => {
+  const navigate = useNavigate(); // Инициализируем useNavigate
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
       <Row gutter={[16, 16]} justify="center">
@@ -20,11 +21,16 @@ const HomePage: React.FC = () => {
           </Typography.Paragraph>
         </Col>
         <Col xs={100} sm={100} md={100} lg={100}>
-          <img
-            src={secondImage} // Замените ссылкой на вашу вторую картинку
-            alt="Second Image"
-            style={{ maxWidth: '100%', maxHeight: '100%', borderRadius: '8px' }}
-          />
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <img
+              src={secondImage} 
+              alt="Second Image"
+              style={{ maxWidth: '100%', maxHeight: '100%', borderRadius: '8px' }}
+            />
+            <Button type="primary" style={{ marginTop: '16px' }} onClick={() => navigate('/catalog')}>
+              Перейти в каталог
+            </Button>
+          </div>
         </Col>
       </Row>
     </div>
